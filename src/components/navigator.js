fa684591b79f3aa8982b8335c4e326ddaddec058
Xiya Liu <xil230@pitt.edu>
2021-09-07 01:27:56 -0700

    navigator

--- src/components/navigator.js
+++ src/components/navigator.js
@@ -1,11 +1,11 @@
 import * as React from "react";
 import { Link } from "gatsby";
-import { slider, link } from "../css/navigator.module.css";
+import { slider, links } from "../css/navigator.module.css";
 import { body } from "../css/theme.module.css";
 
 const Navigator = (props) => {
   return (
-    <Link to={props.link} className={link}>
+    <Link to={props.link} className={links}>
       <span> {props.menu}</span>
       <span className={slider}></span>
     </Link>
