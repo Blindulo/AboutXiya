fa4065362cd0cdfa8abc0601edba237c03733b3b
Xiya Liu <xil230@pitt.edu>
2022-01-08 19:02:22 -0800

    make dragable

--- src/components/bubbles/bubble.js
+++ src/components/bubbles/bubble.js
@@ -1,6 +1,7 @@
 import React, { useState, memo } from "react";
 import { useDrag } from "react-dnd";
-
+import { disappear } from "../../css/bubble.module.css";
+import Draggable from "react-draggable";
 export const Box = memo(function Box({
   name,
   type,
@@ -11,7 +12,6 @@ export const Box = memo(function Box({
   backClass,
   backText1,
   backText2,
-  positionStyle,
 }) {
   const [{ opacity }, drag] = useDrag(
     () => ({
@@ -24,29 +24,39 @@ export const Box = memo(function Box({
     [name, type]
   );
   const [isHover, setText] = useState(false);
+  const handleStart = () => console.log("here");
   return (
-    <div
-      className={frontClass}
-      ref={drag}
-      role="Box"
-      data-testid={`bubble-${name}`}
-      onMouseEnter={() => setText(true)}
-      onMouseLeave={() => setText(false)}
-      style={isDropped ? { display: "none" } : { display: "block" }}
+    <Draggable
+      axis="x"
+      handle=".handle"
+      defaultPosition={{ x: 0, y: 0 }}
+      position={null}
+      grid={[25, 25]}
+      scale={1}
     >
       <div
-        className={textClass}
-        style={isHover ? { display: "none" } : { display: "block" }}
+        className={isDropped ? frontClass + " " + disappear : frontClass}
+        ref={drag}
+        role="Box"
+        data-testid={`bubble-${name}`}
+        onMouseEnter={() => setText(true)}
+        onMouseLeave={() => setText(false)}
+        // style={isDropped ? { display: "none" } : { display: "block" }}
       >
-        <p>{bubbleText}</p>
+        <div
+          className={textClass}
+          style={isHover ? { display: "none" } : { display: "block" }}
+        >
+          <p>{bubbleText}</p>
+        </div>
+        <div
+          className={backClass}
+          style={!isHover ? { display: "none" } : { display: "block" }}
+        >
+          <p>{backText1}</p>
+          <h3>{backText2}</h3>
+        </div>
       </div>
-      <div
-        className={backClass}
-        style={!isHover ? { display: "none" } : { display: "block" }}
-      >
-        <p>{backText1}</p>
-        <h3>{backText2}</h3>
-      </div>
-    </div>
+    </Draggable>
   );
 });
